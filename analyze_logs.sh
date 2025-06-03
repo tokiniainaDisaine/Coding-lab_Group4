d4ce1485c82c82125a899a3b84bd6053a51d60ff
TheRealToky <tokydisaine.andrianarison@gmail.com>
2025-06-03 11:28:21 +0200

    test repo collaboration

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-read -p "Select log to analyze:
+read -p "Select log to analyze: 
 1) Heart Rate
 2) Temperature
 3) Water Usage
-Enter choice (1-3): " user_choice
+Enter choice (1-3): " user_choice # Prompt for the user input
 
