5e599b0178e623e8e3d0c535f90a6b6a96614343
nshderrick056 <d.gatete@alustudent.com>
2025-06-03 12:01:38 -0700

    error handling

--- archive_logs.sh
+++ archive_logs.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Archive directories
+
+HEART_ARCHIVE="hospital_data/archived_logs/heart_data_archives"
+TEMP_ARCHIVE="hospital_data/archived_logs/temperature_data_archives"
+WATER_ARCHIVE="hospital_data/archived_logs/water_usage_data_archives"
+
+# Active logs
+
+HEART_LOG="hospital_data/active_logs/heart_rate_log.log "
+TEMP_LOG="hospital_data/active_logs/temperature_log.log"
+WATER_LOG="hospital_data/active_logs/water_usage_log.log"
+
+# Timestamp format
+
+TIMESTAMP=$(date '+%Y-%m-%d_%H:%M:%S')
+
 read -p "Select log to archive:
 1) Heart Rate
 2) Temperature
@@ -13,4 +29,10 @@ if [[ "user_choice" != "1" && "user_choice" != "2" && "user_choice" != "3" ]]; t
 	exit 1
 fi
 
+# proceed based on the choices
 
+case user_choice in
+
+	
+
+esac
