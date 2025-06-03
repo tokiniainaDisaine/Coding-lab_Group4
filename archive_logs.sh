5428d9383286f807d52220db553d9584c2030d40
nshderrick056 <d.gatete@alustudent.com>
2025-06-03 09:01:17 -0700

    first trial of archive_logs.sh error handling

--- archive_logs.sh
+++ archive_logs.sh
@@ -6,3 +6,11 @@ read -p "Select log to archive:
 3) Water Usage
 Enter choice (1-3): " user_choice
 
+#check if the input is valid
+
+if [[ "user_choice" != "1" && "user_choice" != "2" && "user_choice" != "3" && ]]; then
+	echo " Error: The input is wrong, please select 1, 2, and 3."
+	exit 1
+fi
+
+
